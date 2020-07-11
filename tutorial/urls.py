47ffdc19ff8483865a5fa1394330dc53cb2a93a2
Ricardo Duarte <rickduarte@gmail.com>
2020-07-11 20:53:21 -0300

    completed serializers tutorial

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -25,7 +25,8 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include('tutorial.snippets.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    
+
     path('admin/', admin.site.urls),
 ]
